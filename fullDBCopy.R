6d0fb9d7549bbef77f4567482e208f4549ccc6ff
turbaevsky <turbaevsky@gmail.com>
2018-03-26 16:09:27 +0100

    Add files via upload
    fix DB connection

--- fullDBCopy.R
+++ fullDBCopy.R
@@ -6,10 +6,16 @@ DBCopy <- function(session=TRUE)
 	{
 	print('DB copying...')
 	i <- 0
-	channel <- odbcConnect('SQL Staging Database',uid='PIDBConnect',pwd='Wan0$PIDB')
+        #memory.size(256000)
+	#channel <- odbcConnect('WANO_Staging',uid='PIDBConnect',pwd='Wan0$PIDB')
+        channel <- odbcDriverConnect("Driver={SQL Server};Server=sqllon03\\it;Database=WANO_Staging;uid=PIDBConnect;pwd=Wan0$PIDB;Trusted_Connection=No")
 	tables <- sqlTables(channel,schema='dbo',tableType='TABLE')
-	pb <- txtProgressBar(min = 1, max = length(t(tables[1])), style = 3)
-	for (tn in t(tables[3]))
+        tables <- unlist(tables[3])
+        tbl <- c()
+        for (pi in grep('PI_',tables))
+            tbl <- c(tbl,tables[pi])
+	pb <- txtProgressBar(min = 1, max = length(tbl), style = 3)
+	for (tn in tbl)
 		{
 		i <- i+1
 		fn = paste('DBCopy/',tn,'.rds',sep='')
@@ -20,8 +26,8 @@ DBCopy <- function(session=TRUE)
                 if (session) incProgress(1/length(t(tables[1])),detail=tn)
                 # return(i)
 
-		t<-sqlFetch(channel,tn)
-		saveRDS(t,fn)
+                t<-sqlFetch(channel,tn)
+                saveRDS(t,fn)
 		}
 	close(pb)
 	odbcCloseAll()
